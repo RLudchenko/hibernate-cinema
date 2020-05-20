d1b5f7f4e0e616fd9e7f679ac8d084f5e6b315dd
Ludchenko <skillet367@icloud.com>
2020-05-20 21:15:03 +0300

    added travis file

--- src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
@@ -1,21 +1,26 @@
 package com.dev.cinema.dao.impl;
 
-import java.util.List;
-import javax.persistence.criteria.CriteriaQuery;
+import com.dev.cinema.Main;
 import com.dev.cinema.dao.MovieDao;
 import com.dev.cinema.exceptions.DataProcessingException;
 import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.Movie;
 import com.dev.cinema.util.HibernateUtil;
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     @Override
     public Movie add(Movie movie) {
         Transaction transaction = null;
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             Long itemId = (Long) session.save(movie);
             transaction.commit();
@@ -24,6 +29,8 @@ public class MovieDaoImpl implements MovieDao {
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
+            } else {
+                LOGGER.warn("Can't add Movie", e);
             }
             throw new RuntimeException("Can't insert Movie entity", e);
         }
@@ -31,13 +38,14 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public List<Movie> getAll() {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             CriteriaQuery<Movie> criteriaQuery = session.getCriteriaBuilder()
                     .createQuery(Movie.class);
             criteriaQuery.from(Movie.class);
             return session.createQuery(criteriaQuery).getResultList();
 
         } catch (Exception e) {
+            LOGGER.warn("Error while retrieving all movies.", e);
             throw new DataProcessingException("Error while retrieving all movies.", e);
         }
     }
--- src/main/java/com/dev/cinema/service/MovieServiceImpl.java
+++ src/main/java/com/dev/cinema/service/MovieServiceImpl.java
@@ -1,10 +1,10 @@
 package com.dev.cinema.service;
 
-import java.util.List;
 import com.dev.cinema.dao.MovieDao;
 import com.dev.cinema.lib.Inject;
 import com.dev.cinema.lib.Service;
 import com.dev.cinema.model.Movie;
+import java.util.List;
 
 @Service
 public class MovieServiceImpl implements MovieService {
