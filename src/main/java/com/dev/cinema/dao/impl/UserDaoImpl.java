0de9023be2deaca67b1e8d2ab63e95d512d9dff3
Rostyslav Ludchenko <skillet367@icloud.com>
2020-05-25 19:00:05 +0300

    added user realization together with pass hashing

---
+++ src/main/java/com/dev/cinema/dao/impl/UserDaoImpl.java
@@ -0,0 +1,58 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.UserDao;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import java.util.Optional;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    private static final Logger LOGGER = LogManager.getLogger(UserDaoImpl.class);
+
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            LOGGER.info(session + " was added to DB");
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("There was an error adding "
+                    + session, e);
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<User> criteriaQuery = cb.createQuery(User.class);
+            Root<User> root = criteriaQuery.from(User.class);
+            Predicate predicate = cb
+                    .equal(root.get("email"), email);
+            criteriaQuery.select(root).where(predicate);
+            return Optional.ofNullable(session.createQuery(criteriaQuery).uniqueResult());
+        } catch (Exception e) {
+            throw new DataProcessingException("Failed to retreive user by email ", e);
+        }
+    }
+}
