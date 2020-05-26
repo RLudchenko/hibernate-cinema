da0ff522a4c90625cdca269cfca4365be6cf1be6
Rostyslav Ludchenko <skillet367@icloud.com>
2020-05-26 18:17:24 +0300

    added correct optional and shopping cart classes

---
+++ src/main/java/com/dev/cinema/model/Ticket.java
@@ -0,0 +1,31 @@
+package com.dev.cinema.model;
+
+public class Tickets {
+    private Long id;
+    private User user;
+    private MovieSession movieSession;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+}
---
+++ src/main/java/com/dev/cinema/service/ShoppingCartService.java
@@ -0,0 +1,4 @@
+package com.dev.cinema.service;
+
+public class ShoppingCartService {
+}
