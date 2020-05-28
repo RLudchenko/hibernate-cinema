5cfb46fa48ce007bd440084fe76fe422f2309467
Rostyslav Ludchenko <skillet367@icloud.com>
2020-05-28 18:35:25 +0300

    removed constructor and changed OrderServiceImpl to my better understanding

--- src/main/java/com/dev/cinema/service/impl/OrderServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/OrderServiceImpl.java
@@ -4,9 +4,11 @@ import com.dev.cinema.dao.OrderDao;
 import com.dev.cinema.lib.Inject;
 import com.dev.cinema.lib.Service;
 import com.dev.cinema.model.Order;
+import com.dev.cinema.model.ShoppingCart;
 import com.dev.cinema.model.Ticket;
 import com.dev.cinema.model.User;
 import com.dev.cinema.service.OrderService;
+import com.dev.cinema.service.ShoppingCartService;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -14,10 +16,19 @@ import java.util.List;
 public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(List<Ticket> tickets, User user) {
-        return orderDao.add(new Order(List.copyOf(tickets), user, LocalDateTime.now()));
+        Order order = new Order();
+        order.setUser(user);
+        order.setTickets(tickets);
+        order.setOrderDate(LocalDateTime.now());
+        Order orderFromDb = orderDao.add(order);
+        ShoppingCart shoppingCart = shoppingCartService.getByUser(user);
+        shoppingCartService.clear(shoppingCart);
+        return orderFromDb;
     }
 
     @Override
