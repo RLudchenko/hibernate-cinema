f90ec1f570c86616f4a9dd068c41e975acd144a1
Rostyslav Ludchenko <skillet367@icloud.com>
2020-06-09 18:30:49 +0300

    removed views from Web-Config

--- src/main/java/com/dev/cinema/config/WebConfig.java
+++ src/main/java/com/dev/cinema/config/WebConfig.java
@@ -16,8 +16,6 @@ public class WebConfig {
         InternalResourceViewResolver resolver
                 = new InternalResourceViewResolver();
         resolver.setViewClass(JstlView.class);
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
         return resolver;
     }
 }
